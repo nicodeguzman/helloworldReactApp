4837656c0527293f3c8f046713acf37dca0efec9
nicodeguzman <90684659+nicodeguzman@users.noreply.github.com>
2026-04-08 19:40:50 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const element = React.createElement(
+    'h1',
+    {
+        className: 'title',
+        onClick: () => { console.log('clicked'); },
+        style: { fontFamily: 'sans-serif', color: 'rgb(255, 100, 100)' }
+    },
+    'Hello World!'
+);
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(element);
+
+const container2 = document.getElementById('root2');
+const root2 = ReactDOM.createRoot(container2);
+root2.render(element);
+
+document.querySelectorAll('.button').forEach(domContainer => {
+    const messageId = parseInt(domContainer.dataset.messageId);
+
+    const root = ReactDOM.createRoot(domContainer);
+
+    root.render(
+        React.createElement(
+            'button',
+            {
+                onClick: () => { console.log(`liked message: ${messageId}`); }
+            },
+            'Like'
+        )
+    );
+});
